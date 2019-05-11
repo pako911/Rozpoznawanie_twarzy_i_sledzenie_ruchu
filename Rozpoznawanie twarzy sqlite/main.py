e87dc1cfedfbfa0129c1eec73af5c04b200ed732
remeq7 <remremo20a@gmail.com>
2019-05-11 10:59:19 +0200

    Wykrywanie obiektu

--- Rozpoznawanie twarzy sqlite/main.py
+++ Rozpoznawanie twarzy sqlite/main.py
@@ -200,10 +200,50 @@ def ip_camera_face_recognition():
         cv2.imshow('test', img)
         if cv2.waitKey(1) == ord('q'):
             break
+def detecting_object():
+    lowerBound = np.array([110,50,50])
+    upperBound = np.array([130,255,255])
+
+    cam = cv2.VideoCapture(0)
+    kernelOpen = np.ones((5,5))
+    kernelClose = np.ones((20,20))
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    while True:
+        ret,img = cam.read()
+        img = cv2.resize(img,(340,220))
+
+        #convert BGR to HSV
+        imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+        #create the Mask
+        mask = cv2.inRange(imgHSV, lowerBound,upperBound)
+
+        #morphology
+        maskOpen = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+        maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
+
+        maskFinal = maskClose
+
+        conts,h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        cv2.drawContours(img,conts,-1,(255,0,0),3)
+
+        for i in range(len(conts)):
+            x,y,w,h = cv2.boundingRect(conts[i])
+            cv2.rectangle(img, (x,y), (x+w,y+h),(0,0,255),2)
+            cv2.putText(img,str(i+1),(x,y+h),font,1,(0,255,255))
+        cv2.imshow("maskClose", maskClose)
+        cv2.imshow("maskOpen", maskOpen)
+        cv2.imshow("mask", mask)
+        cv2.imshow("cam", img)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 
 if __name__ == "__main__":
-     face_detection()
+     #face_detection()
     # create_data_set()
     # get_images_with_id(file_name)
     # create_training_file()
@@ -211,6 +251,7 @@ if __name__ == "__main__":
     # ip_camera()
     # ip_camera_face_detection()
     #ip_camera_face_recognition()
+    detecting_object() #wykrywanie niebieskiego obiektu
 
     #taktyczny komentarz
 
