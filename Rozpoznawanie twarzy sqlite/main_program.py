63337aed69902591adc4ea4f1f5b6620be1ac512
pako911 <pawelkorczak911@gmail.com>
2019-10-09 20:28:30 +0200

    Permissions

--- Rozpoznawanie twarzy sqlite/main_program.py
+++ Rozpoznawanie twarzy sqlite/main_program.py
@@ -4,8 +4,6 @@ import os
 import sqlite3
 import sys
 import urllib.request
-import recognizer
-from dbhandler import DBHandler
 
 from PIL import Image
 from PyQt5 import QtWidgets, QtCore, QtGui
@@ -21,10 +19,11 @@ class MyForm(QtWidgets.QMainWindow):
         self.ui.pushButton.clicked.connect(self.face_recognition)
         self.ui.pushButton_2.clicked.connect(self.db_display)
         self.ui.ipCameraButton.clicked.connect(self.ip_camera_face_recognition)
-        self.ui.pushButton_6.clicked.connect(self.motion_detector_cam)
+        # self.ui.pushButton_6.clicked.connect(self.motion_detector_cam)
         self.ui.pushButton_3.clicked.connect(self.db_edit)
         self.ui.pushButton_3_2.clicked.connect(self.delete_user)
-        # self.ui.pushButton_3_3.clicked.connect(self.) po kropce dodaj nazwę funkcji bez nawiasów
+        self.ui.pushButton_3_3.clicked.connect(self.delete_permissions)
+        self.ui.pushButton_3_4.clicked.connect(self.add_permissions)
         self.ui.pushButton_4.clicked.connect(self.create_data_set)
         self.ui.pushButton_5.clicked.connect(self.create_training_file)
 
@@ -52,8 +51,7 @@ class MyForm(QtWidgets.QMainWindow):
                     self.msg.exec_()
                     while True:
                         ret, img = cam.read()
-                        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                        faces = face_detect.detectMultiScale(gray, 1.2, 2)
+                        faces = face_detect.detectMultiScale(img, 1.2, 2)
                         for (x, y, w, h) in faces:
                             sample_num = sample_num + 1
                             conn = sqlite3.connect("FaceBaseGit.db")
@@ -61,12 +59,12 @@ class MyForm(QtWidgets.QMainWindow):
                             max_id = cursor.fetchone()[0]
                             id = max_id
                             cv2.imwrite("dataSet/User." + str(id) + "." + str(sample_num) + ".jpg",
-                                        gray[y:y + h, x:x + w])
+                                        img[y:y + h, x:x + w])
                             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                            cv2.waitKey(100)
+                            cv2.waitKey(20)
                         cv2.imshow("Face", img)
                         cv2.waitKey(1)
-                        if sample_num > 20:  # ilość zdjęć do datasetu
+                        if sample_num > 50:  # ilość zdjęć do datasetu
                             break
                     cam.release()
                     cv2.destroyAllWindows()
@@ -83,7 +81,6 @@ class MyForm(QtWidgets.QMainWindow):
             face_np = np.array(face_img, 'uint8')
             _id = int(os.path.split(imagePath)[-1].split('.')[1])
             faces.append(face_np)
-            # print(_id)
             ids.append(_id)
             cv2.imshow("training", face_np)
             cv2.waitKey(10)
@@ -98,6 +95,16 @@ class MyForm(QtWidgets.QMainWindow):
     def get_profile(self, _id):
         conn = sqlite3.connect("FaceBaseGit.db")
         cmd = "SELECT * FROM People WHERE ID=" + str(_id)
+        cursor = conn.execute(cmd)
+        profile = None
+        for row in cursor:
+            profile = row
+        conn.close()
+        return profile
+
+    def get_permission(self, _id, cameraid):
+        conn = sqlite3.connect("FaceBaseGit.db")
+        cmd = "SELECT * FROM Permissions WHERE PersonID=" + str(_id) + " AND CameraID="+ str(cameraid)
         cursor = conn.execute(cmd)
         profile = None
         for row in cursor:
@@ -122,7 +129,7 @@ class MyForm(QtWidgets.QMainWindow):
             if ok:
                 self.qbox.setText(str(video_name))
                 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-                video = cv2.VideoWriter(video_name + '.avi', fourcc, 20.0, (640, 480))
+                video = cv2.VideoWriter(video_name + '.avi', fourcc, 5.0, (640, 480))
             else:
                 record = False
         else:
@@ -136,19 +143,27 @@ class MyForm(QtWidgets.QMainWindow):
             i = i / 4
             cv2.putText(img, "number of people: " + str(i), (10, 50), font_face, 0.7, (255, 255, 0), 2)
             for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 _id, conf = rec.predict(gray[y:y + h, x:x + w])
                 profile = self.get_profile(_id)
+                profile2 = self.get_permission(_id,1)
 
                 if profile is not None:
-                    if conf > 60:  # prawdopodobieństwo poprawnego wykrycia twarzy (im niższa liczba tym jest ono większe)
-                        cv2.putText(img, "unknown", (x, y + h + 30), font_face, 1, (255, 0, 0), 2)
+                    if conf > 60:# prawdopodobieństwo poprawnego wykrycia twarzy (im niższa liczba tym jest ono większe)
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                        cv2.putText(img, "unknown", (x, y + h + 30), font_face, 1, (0, 0, 255), 2)
                     else:
+
                         _id, conf = rec.predict(gray[y:y + h, x:x + w])
                         profile = self.get_profile(_id)
-                        cv2.putText(img, str(profile[1]), (x, y + h + 30), font_face, 1, (255, 0, 0), 2)
-                        cv2.putText(img, str(profile[2]), (x, y + h + 60), font_face, 1, (255, 0, 0), 2)
-                        cv2.putText(img, str(profile[3]), (x, y + h + 90), font_face, 1, (255, 0, 0), 2)
+                        if profile2 is not None:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                            cv2.putText(img, str(profile[1]), (x, y + h + 30), font_face, 1, (0, 255, 0), 2)
+                        else:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                            cv2.putText(img, str(profile[1]), (x, y + h + 30), font_face, 1, (0, 0, 255), 2)
+                        # cv2.putText(img, str(profile[2]), (x, y + h + 60), font_face, 1, (255, 0, 0), 2)
+                        # cv2.putText(img, str(profile[3]), (x, y + h + 90), font_face, 1, (255, 0, 0), 2)
+
             cv2.putText(img, "Press [ESC] to exit", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             cv2.imshow("Main camera view", img)
             if record:
@@ -195,20 +210,24 @@ class MyForm(QtWidgets.QMainWindow):
             img = cv2.imdecode(img_np, -1)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = face_detect.detectMultiScale(gray, 1.05, 5)
-            blabla = np.array(faces)
-            i = blabla.size
-            i = i / 4
-            cv2.putText(img, "number of people: " + str(i), (10, 50), font_face, 1, (255, 255, 0), 2)
 
             for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 _id, conf = rec.predict(gray[y:y + h, x:x + w])
                 profile = self.get_profile(_id)
+                profile2 = self.get_permission(_id, 2)
                 if profile is not None:
-                    cv2.putText(img, str(profile[1]), (x, y + h + 30), font_face, 1, (255, 0, 0), 2)
-                    # zamist str(id) -> profile
-                    cv2.putText(img, str(profile[2]), (x, y + h + 60), font_face, 1, (255, 0, 0), 2)
-                    cv2.putText(img, str(profile[3]), (x, y + h + 90), font_face, 1, (255, 0, 0), 2)
+                    if conf > 60:# prawdopodobieństwo poprawnego wykrycia twarzy (im niższa liczba tym jest ono większe)
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                        cv2.putText(img, "unknown", (x, y + h + 30), font_face, 1, (0, 0, 255), 2)
+                    else:
+                        _id, conf = rec.predict(gray[y:y + h, x:x + w])
+                        profile = self.get_profile(_id)
+                        if profile2 is not None:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                            cv2.putText(img, str(profile[1]), (x, y + h + 30), font_face, 1, (0, 255, 0), 2)
+                        else:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                            cv2.putText(img, str(profile[1]), (x, y + h + 30), font_face, 1, (0, 0, 255), 2)
             cv2.putText(img, "Press [ESC] to exit", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             cv2.imshow("IP camera view", img)
             if record:
@@ -236,15 +255,47 @@ class MyForm(QtWidgets.QMainWindow):
 
     def db_display(self):
         conn = sqlite3.connect("FaceBaseGit.db")
-        query = "SELECT * FROM People"
-        result = conn.execute(query)
+        choice, ok = QtWidgets.QInputDialog.getItem(self, 'Table choice', 'Select table to display',
+                                                    ('People', 'Cameras', 'Permissions'), 0, False)
+        if ok:
+            if choice == 'People':
+                self.ui.tableWidget2.hide()
+                self.ui.tableWidget3.hide()
+                query = "SELECT * FROM People"
+                result = conn.execute(query)
+                self.ui.tableWidget.setRowCount(0)
+                self.ui.tableWidget.show()
+                for row_number, row_data in enumerate(result):
+                    self.ui.tableWidget.insertRow(row_number)
+                    for column_number, data in enumerate(row_data):
+                        self.ui.tableWidget.setItem(row_number, column_number,
+                                                    QtWidgets.QTableWidgetItem(str(data)))
+            elif choice == 'Cameras':
+                self.ui.tableWidget.hide()
+                self.ui.tableWidget3.hide()
+                query = "SELECT * FROM Cameras"
+                result = conn.execute(query)
+                self.ui.tableWidget2.setRowCount(0)
+                self.ui.tableWidget2.show()
+                for row_number, row_data in enumerate(result):
+                    self.ui.tableWidget2.insertRow(row_number)
+                    for column_number, data in enumerate(row_data):
+                        self.ui.tableWidget2.setItem(row_number, column_number,
+                                                    QtWidgets.QTableWidgetItem(str(data)))
+            elif choice == 'Permissions':
+                self.ui.tableWidget.hide()
+                self.ui.tableWidget2.hide()
+                query = "SELECT People.ID, People.Name, Cameras.Room FROM People, Permissions " \
+                        "INNER JOIN Cameras ON People.ID = Permissions.PersonID " \
+                        "AND Cameras.CameraID = Permissions.CameraID ORDER BY ID"
+                result = conn.execute(query)
+                self.ui.tableWidget3.show()
+                for row_number, row_data in enumerate(result):
+                    self.ui.tableWidget3.insertRow(row_number)
+                    for column_number, data in enumerate(row_data):
+                        self.ui.tableWidget3.setItem(row_number, column_number,
+                                                    QtWidgets.QTableWidgetItem(str(data)))
 
-        self.ui.tableWidget.setRowCount(0)
-        for row_number, row_data in enumerate(result):
-            self.ui.tableWidget.insertRow(row_number)
-            for column_number, data in enumerate(row_data):
-                self.ui.tableWidget.setItem(row_number, column_number,
-                                            QtWidgets.QTableWidgetItem(str(data)))
         conn.close()
 
     def db_update(self, _id, name, age, gender):  # funkcja do modyfikowania bazy danych
@@ -312,6 +363,44 @@ class MyForm(QtWidgets.QMainWindow):
         conn.commit()
         conn.close()
 
+    def delete_permissions(self):
+        self.qbox = QtWidgets.QLineEdit()
+        _id, ok = QtWidgets.QInputDialog.getInt(self, ' ', 'Enter id')
+        conn = sqlite3.connect("FaceBaseGit.db")
+        if ok:
+            cmd = "SELECT * FROM Permissions WHERE PersonID =" + str(_id)
+            cursor = conn.execute(cmd)
+            does_record_exists = 0
+
+            for _ in cursor:
+                does_record_exists = 1
+            if does_record_exists == 1:
+                cmd = "DELETE FROM Permissions WHERE PersonID= " + str(_id)
+
+        conn.execute(cmd)
+        conn.commit()
+        conn.close()
+
+    def add_permissions(self):
+        self.qbox = QtWidgets.QLineEdit()
+        _id, ok = QtWidgets.QInputDialog.getInt(self, ' ', 'Enter id')
+        conn = sqlite3.connect("FaceBaseGit.db")
+        cmd = "SELECT * FROM Permissions WHERE PersonID =" + str(_id)
+        # cursor = conn.execute(cmd)
+        cursor2 = conn.execute("SELECT MAX(PermissionID) FROM Permissions")
+        max_id = cursor2.fetchone()[0]
+        if max_id != None:
+            per_id = max_id + 1
+        else:
+            per_id = 1
+        camera_id, ok = QtWidgets.QInputDialog.getInt(self, ' ', 'Enter camera id')
+        if ok:
+            cmd = "INSERT INTO Permissions(PermissionID, PersonID, CameraID) VALUES(" + str(per_id) + "," + str(_id) + "," \
+                  + str(camera_id) + ")"
+
+        conn.execute(cmd)
+        conn.commit()
+        conn.close()
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
